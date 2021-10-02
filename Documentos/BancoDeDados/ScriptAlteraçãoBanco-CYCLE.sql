7e701fc41182c05ee23e05dba36410fe8bce5789
carloslimasousa <79268209+carloslimasousa@users.noreply.github.com>
2021-10-02 15:48:16 -0300

    refactor: Mudanças nos campos do DER

--- Documentos/BancoDeDados/ScriptAlteraçãoBanco-CYCLE.sql
+++ Documentos/BancoDeDados/ScriptAlteraçãoBanco-CYCLE.sql
@@ -1,23 +1,22 @@
--- Adição de foreign key na tabela unidade
-ALTER TABLE tbl_unidade ADD COLUMN id_matriz INT;
-ALTER TABLE tbl_unidade ADD FOREIGN KEY (id_matriz) REFERENCES tbl_matriz(id_matriz);
-ALTER TABLE tbl_unidade ADD COLUMN id_codigo_uf INT;
+-- Configurando foreign key da tabela unidade
+ALTER TABLE tbl_unidade ADD FOREIGN KEY (id_usuario_contratante) REFERENCES tbl_usuario_contratante(id_usuario_contratante);
 ALTER TABLE tbl_unidade ADD FOREIGN KEY (id_codigo_uf) REFERENCES tbl_codigo_uf(id_codigo_uf);
 
--- Adição de foreign key na tabela matriz
-ALTER TABLE tbl_matriz ADD COLUMN id_codigo_uf INT;
-ALTER TABLE tbl_matriz ADD FOREIGN KEY (id_codigo_uf) REFERENCES tbl_codigo_uf(id_codigo_uf);
+-- Configurando foreign key da tabela contratante
+ALTER TABLE tbl_usuario_contratante ADD FOREIGN KEY (id_codigo_uf) REFERENCES tbl_codigo_uf(id_codigo_uf);
+ALTER TABLE tbl_usuario_contratante ADD FOREIGN KEY (id_plano) REFERENCES tbl_plano(id_plano);
 
--- Adição de foreign key na tabela login suporte
-ALTER TABLE tbl_login_suporte ADD COLUMN id_matriz INT;
-ALTER TABLE tbl_login_suporte ADD FOREIGN KEY (id_matriz) REFERENCES tbl_matriz(id_matriz);
-ALTER TABLE tbl_login_suporte ADD COLUMN id_supervisor INT;
-ALTER TABLE tbl_login_suporte ADD FOREIGN KEY (id_supervisor) REFERENCES tbl_login_suporte(id_sup);
+-- Configurando foreign key da tabela usuario
+ALTER TABLE tbl_usuario_suporte ADD FOREIGN KEY (id_usuario_contratante) REFERENCES tbl_usuario_contratante(id_usuario_contratante);
 
--- Adição de foreign key na tabela maquina
-ALTER TABLE tbl_maquina ADD COLUMN id_unidade INT;
+-- Configurando foreign key da tabela maquina
 ALTER TABLE tbl_maquina ADD FOREIGN KEY (id_unidade) REFERENCES tbl_unidade(id_unidade);
 
--- Adição de foreign key na tabela registro
-ALTER TABLE tbl_registro ADD COLUMN id_maquina INT;
-ALTER TABLE tbl_registro ADD FOREIGN KEY (id_maquina) REFERENCES tbl_maquina(id_maquina);
+-- Configurando foreign key da tabela registro ram
+ALTER TABLE tbl_registro_ram ADD FOREIGN KEY (id_maquina) REFERENCES tbl_maquina(id_maquina);
+
+-- Configurando foreign key da tabela registro cpu
+ALTER TABLE tbl_registro_cpu ADD FOREIGN KEY (id_maquina) REFERENCES tbl_maquina(id_maquina);
+
+-- Configurando foreign key da tabela registro disco
+ALTER TABLE tbl_registro_disco ADD FOREIGN KEY (id_maquina) REFERENCES tbl_maquina(id_maquina);
--- Documentos/BancoDeDados/ScriptCriaçãoBanco-CYCLE.sql
+++ Documentos/BancoDeDados/ScriptCriaçãoBanco-CYCLE.sql
@@ -1,48 +1,81 @@
-CREATE DATABASE cycle;
+CREATE DATABASE IF NOT EXISTS cycle;
+
 USE cycle;
 
-CREATE TABLE tbl_unidade (
-id_unidade INT PRIMARY KEY AUTO_INCREMENT,
-nome_unidade VARCHAR(60),
-endereco_unidade VARCHAR(255),
-telefone_unidade VARCHAR(14),
-cnpj_unidade VARCHAR(14)
+CREATE TABLE IF NOT EXISTS tbl_unidade (
+	id_unidade INT PRIMARY KEY,
+	nome_unidade VARCHAR(60),
+	endereco_unidade VARCHAR(255),
+	telefone_unidade VARCHAR(14),
+	cnpj_unidade VARCHAR(14),
+	id_usuario_contratante INT,
+	id_codigo_uf INT
 );
 
-CREATE TABLE tbl_codigo_uf (
-id_codigo_uf INT PRIMARY KEY AUTO_INCREMENT,
-desc_codigo_uf VARCHAR(2)
+CREATE TABLE IF NOT EXISTS tbl_usuario_contratante(
+	id_usuario_contratante INT PRIMARY KEY,
+	nome_contratante VARCHAR(80),
+	email_contratante VARCHAR(80),
+	senha_contratante VARCHAR(20),
+	cnpf_contratante VARCHAR(14),
+	id_codigo_uf INT,
+	id_plano INT
 );
 
-CREATE TABLE tbl_matriz (
-id_matriz INT PRIMARY KEY AUTO_INCREMENT,
-nome_matriz VARCHAR(70),
-endereco_matriz VARCHAR(255),
-cnpj_matriz VARCHAR(14)
+CREATE TABLE IF NOT EXISTS tbl_usuario_suporte (
+	id_sup INT PRIMARY KEY,
+	nome_suporte VARCHAR(80),
+	login_suporte VARCHAR(30),
+	senha_suporte VARCHAR(20),
+	id_usuario_contratante INT
 );
 
-CREATE TABLE tbl_login_suporte (
-id_sup INT PRIMARY KEY AUTO_INCREMENT,
-nome_sup VARCHAR(80),
-login_sup VARCHAR(30),
-senha_sup VARCHAR(20)
+CREATE TABLE IF NOT EXISTS tbl_codigo_uf (
+	id_codigo_uf INT PRIMARY KEY,
+	desc_codigo_uf varchar(2)
 );
 
-CREATE TABLE tbl_maquina (
-id_maquina INT PRIMARY KEY AUTO_INCREMENT,
-nome_maquina VARCHAR(30),
-sistema_operacional_maquina VARCHAR(15),
-modelo_cpu VARCHAR(50),
-qnt_memoria_ram VARCHAR(5),
-qnt_disco VARCHAR(6),
-tipo_maquina VARCHAR(11)
-CHECK (tipo_maquina = "totem" or tipo_maquina = "atendimento")
+CREATE TABLE IF NOT EXISTS tbl_plano (
+	id_plano INT PRIMARY KEY,
+	nome_plano VARCHAR(20),
+	qnt_maquinas FLOAT,
+	valor_plano FLOAT
 );
 
-CREATE TABLE tbl_registro (
-id_registro INT PRIMARY KEY AUTO_INCREMENT,
-cpu_maquina FLOAT,
-memoria_ram_maquina FLOAT,
-disco_maquina FLOAT,
-data_registro DATETIME
+CREATE TABLE IF NOT EXISTS tbl_maquina (
+	id_maquina INT PRIMARY KEY,
+	apelido_maquina VARCHAR(30),
+	sistema_operacional_maquina VARCHAR(15),
+	modelo_cpu VARCHAR(50),
+	id_processador VARCHAR(20),
+	tipo_maquina VARCHAR(10),
+	id_unidade INT
+);
+
+CREATE TABLE IF NOT EXISTS tbl_registro_ram (
+	id_registro_ram INT PRIMARY KEY,
+	memoria_ram_total FLOAT,
+	memoria_ram_em_uso FLOAT,
+	memoria_ram_livre FLOAT,
+	data_hora_registro DATETIME,
+	id_maquina INT
+);
+
+CREATE TABLE IF NOT EXISTS tbl_registro_cpu (
+	id_registro_cpu INT PRIMARY KEY,	
+	cpu_em_uso FLOAT,
+	cpu_frequencia FLOAT,
+	temperatura_cpu FLOAT,
+	data_hora_registro DATETIME,
+	id_maquina INT
+);
+
+CREATE TABLE IF NOT EXISTS tbl_registro_disco (
+	id_registro_disco INT PRIMARY KEY, 
+	tipo_disco VARCHAR(4),
+	espaco_total_disco FLOAT,
+	espaco_ocupado_disco FLOAT,
+	espaco_livre_disco FLOAT,
+	data_hora_registro DATETIME,
+	id_maquina INT
 );
