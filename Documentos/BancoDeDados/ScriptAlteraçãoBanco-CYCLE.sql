a09c287e5363c742ce4ea79f0cceb7d42de18ab2
carloslimasousa <carlos.lsousa3@outlook.com>
2021-10-09 14:58:20 -0300

    Refactor: Mudanças do script MySql com base no novo DER

--- Documentos/BancoDeDados/ScriptAlteraçãoBanco-CYCLE.sql
+++ Documentos/BancoDeDados/ScriptAlteraçãoBanco-CYCLE.sql
@@ -1,22 +1,15 @@
 -- Configurando foreign key da tabela unidade
-ALTER TABLE tbl_unidade ADD FOREIGN KEY (id_usuario_contratante) REFERENCES tbl_usuario_contratante(id_usuario_contratante);
-ALTER TABLE tbl_unidade ADD FOREIGN KEY (id_codigo_uf) REFERENCES tbl_codigo_uf(id_codigo_uf);
+ALTER TABLE tblUnidade ADD FOREIGN KEY (idUsuarioContratante) REFERENCES tblUsuarioContratante(idUsuarioContratante);
+ALTER TABLE tblUnidade ADD FOREIGN KEY (idCodigoUf) REFERENCES tblCodigoUf(idCodigoUf);
 
 -- Configurando foreign key da tabela contratante
-ALTER TABLE tbl_usuario_contratante ADD FOREIGN KEY (id_codigo_uf) REFERENCES tbl_codigo_uf(id_codigo_uf);
-ALTER TABLE tbl_usuario_contratante ADD FOREIGN KEY (id_plano) REFERENCES tbl_plano(id_plano);
+ALTER TABLE tblUsuarioContratante ADD FOREIGN KEY (idPlano) REFERENCES tblPlano(idPlano);
 
--- Configurando foreign key da tabela usuario
-ALTER TABLE tbl_usuario_suporte ADD FOREIGN KEY (id_usuario_contratante) REFERENCES tbl_usuario_contratante(id_usuario_contratante);
+-- Configurando foreign key da tabela usuario suporte
+ALTER TABLE tblUsuarioSuporte ADD FOREIGN KEY (idUsuarioContratante) REFERENCES tblUsuarioContratante(idUsuarioContratante);
 
 -- Configurando foreign key da tabela maquina
-ALTER TABLE tbl_maquina ADD FOREIGN KEY (id_unidade) REFERENCES tbl_unidade(id_unidade);
+ALTER TABLE tblMaquina ADD FOREIGN KEY (idUnidade) REFERENCES tblUnidade(idUnidade);
 
--- Configurando foreign key da tabela registro ram
-ALTER TABLE tbl_registro_ram ADD FOREIGN KEY (id_maquina) REFERENCES tbl_maquina(id_maquina);
-
--- Configurando foreign key da tabela registro cpu
-ALTER TABLE tbl_registro_cpu ADD FOREIGN KEY (id_maquina) REFERENCES tbl_maquina(id_maquina);
-
--- Configurando foreign key da tabela registro disco
-ALTER TABLE tbl_registro_disco ADD FOREIGN KEY (id_maquina) REFERENCES tbl_maquina(id_maquina);
+-- Configurando foreign key da tabela registro
+ALTER TABLE tblRegistro ADD FOREIGN KEY (idMaquina) REFERENCES tblMaquina(idMaquina);
--- Documentos/BancoDeDados/ScriptCriaçãoBanco-CYCLE.sql
+++ Documentos/BancoDeDados/ScriptCriaçãoBanco-CYCLE.sql
@@ -2,80 +2,65 @@ CREATE DATABASE IF NOT EXISTS cycle;
 
 USE cycle;
 
-CREATE TABLE IF NOT EXISTS tbl_unidade (
-	id_unidade INT PRIMARY KEY,
-	nome_unidade VARCHAR(60),
-	endereco_unidade VARCHAR(255),
-	telefone_unidade VARCHAR(14),
-	cnpj_unidade VARCHAR(14),
-	id_usuario_contratante INT,
-	id_codigo_uf INT
+CREATE TABLE tblUnidade (
+	idUnidade INT PRIMARY KEY,
+	nomeUnidade VARCHAR(60),
+	enderecoUnidade VARCHAR(255),
+	telefoneUnidade VARCHAR(14),
+	cnpjUnidade VARCHAR(14),
+	idUsuarioContratante INT,
+	idCodigoUf INT
 );
 
-CREATE TABLE IF NOT EXISTS tbl_usuario_contratante(
-	id_usuario_contratante INT PRIMARY KEY,
-	nome_contratante VARCHAR(80),
-	email_contratante VARCHAR(80),
-	senha_contratante VARCHAR(20),
-	cnpf_contratante VARCHAR(14),
-	id_codigo_uf INT,
-	id_plano INT
+CREATE TABLE tblUsuarioContratante(
+	idUsuarioContratante INT PRIMARY KEY,
+	nomeContratante VARCHAR(80),
+	emailContratante VARCHAR(80),
+	senhaContratante VARCHAR(20),
+	cnpfContratante VARCHAR(14),
+	idPlano INT
 );
 
-CREATE TABLE IF NOT EXISTS tbl_usuario_suporte (
-	id_sup INT PRIMARY KEY,
-	nome_suporte VARCHAR(80),
-	login_suporte VARCHAR(30),
-	senha_suporte VARCHAR(20),
-	id_usuario_contratante INT
+CREATE TABLE tblUsuarioSuporte (
+	idSup INT PRIMARY KEY,
+	nomeSuporte VARCHAR(80),
+	loginSuporte VARCHAR(30),
+	senhaSuporte VARCHAR(20),
+	idUsuarioContratante INT
 );
 
-CREATE TABLE IF NOT EXISTS tbl_codigo_uf (
-	id_codigo_uf INT PRIMARY KEY,
-	desc_codigo_uf varchar(2)
+CREATE TABLE IF NOT EXISTS tblCodigoUf (
+	idCodigoUf INT PRIMARY KEY,
+	descCodigoUf varchar(2)
 );
 
-CREATE TABLE IF NOT EXISTS tbl_plano (
-	id_plano INT PRIMARY KEY,
-	nome_plano VARCHAR(20),
-	qnt_maquinas FLOAT,
-	valor_plano FLOAT
+CREATE TABLE tblPlano (
+	idPlano INT PRIMARY KEY,
+	nomePlano VARCHAR(20),
+	qntMaquinas INT,
+	valorPlano DOUBLE
 );
 
-CREATE TABLE IF NOT EXISTS tbl_maquina (
-	id_maquina INT PRIMARY KEY,
-	apelido_maquina VARCHAR(30),
-	sistema_operacional_maquina VARCHAR(15),
-	modelo_cpu VARCHAR(50),
-	id_processador VARCHAR(20),
-	tipo_maquina VARCHAR(10),
-	id_unidade INT
+CREATE TABLE tblMaquina (
+	idMaquina INT PRIMARY KEY,
+	apelidoMaquina VARCHAR(30),
+    tipoMaquina VARCHAR(10),
+	sistemaOperacionalMaquina VARCHAR(15),
+    idProcessador VARCHAR(20),
+	modeloCpu VARCHAR(50),
+    cpuFrequencia DOUBLE,
+    tipoDisco VARCHAR(4),
+    espacoTotalDisco DOUBLE,
+    espacoTotalRam DOUBLE,
+	idUnidade INT
 );
 
-CREATE TABLE IF NOT EXISTS tbl_registro_ram (
-	id_registro_ram INT PRIMARY KEY,
-	memoria_ram_total FLOAT,
-	memoria_ram_em_uso FLOAT,
-	memoria_ram_livre FLOAT,
-	data_hora_registro DATETIME,
-	id_maquina INT
-);
-
-CREATE TABLE IF NOT EXISTS tbl_registro_cpu (
-	id_registro_cpu INT PRIMARY KEY,	
-	cpu_em_uso FLOAT,
-	cpu_frequencia FLOAT,
-	temperatura_cpu FLOAT,
-	data_hora_registro DATETIME,
-	id_maquina INT
-);
-
-CREATE TABLE IF NOT EXISTS tbl_registro_disco (
-	id_registro_disco INT PRIMARY KEY, 
-	tipo_disco VARCHAR(4),
-	espaco_total_disco FLOAT,
-	espaco_ocupado_disco FLOAT,
-	espaco_livre_disco FLOAT,
-	data_hora_registro DATETIME,
-	id_maquina INT
+CREATE TABLE tblRegistro (
+	idregistro INT PRIMARY KEY,
+    cpuEmUso DOUBLE,
+    temperaturaCpu DOUBLE,
+    espacoOcupadoDisco DOUBLE,
+    espacoOcupadoRam DOUBLE,
+    dataHoraRegistro DATETIME,
+    idMaquina INT
 );
