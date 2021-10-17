1eda2de2de8e7a6abd04da6033b1d234cb9e7800
eliasnachle <elias.nachle@bandtec.com.br>
2021-10-17 04:45:57 -0300

    feature: adicionando fundo ao abrir menu

--- projeto-site/public/js/dashboard-support/menu.js
+++ projeto-site/public/js/dashboard-support/menu.js
@@ -1,6 +1,4 @@
 const sidebar_logo = document.querySelector('.sidebar__content--logo > img'),
-mobileMenuOptions = document.querySelector('.mobile-menu-options'),
-handlerClickProfile = document.querySelector('.mobile-menu__content > a:nth-child(5) > img'),
 mobileMenu = document.querySelector('.mobile-menu');
 
 window.addEventListener('resize', () => {
@@ -12,9 +10,16 @@ window.addEventListener('resize', () => {
 })
   
 document.onclick = function(e){
-  if(e.target === handlerClickProfile){
+  const mobileMenuOptions = document.querySelector('.mobile-menu-options');
+  if(e.target.id == 'profileMenu'){
     mobileMenuOptions.style.bottom = '0';
+    const body = document.getElementsByTagName('BODY')[0];
+    body.innerHTML += `<div class="overlay"></div>`;
   } else{
     mobileMenuOptions.style.bottom = '-100%';
+    if(document.querySelector('.overlay') !== null ){
+      const t = document.querySelector('.overlay');
+      t.parentNode.removeChild(t);
+    }
   }
 }
