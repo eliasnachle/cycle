0a2dddd366f5ffba9f5b23f95e69c9b20f643ac6
eliasnachle <elias.nachle@bandtec.com.br>
2021-10-20 21:33:20 -0300

    refactor: corrigindo nome de pasta

---
+++ projeto-site/public/js/dash_user.js
@@ -0,0 +1,8 @@
+//alterar seleção na dash do administrador
+const linkColor = document.querySelectorAll('.nav-link');   
+function colorLink(){
+    linkColor.forEach(i => i.classList.remove('active'));
+    this.classList.add('active');
+}
+
+linkColor.forEach(i => i.addEventListener('click', colorLink));
