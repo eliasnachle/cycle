158a00d2eb5bda036aa06cda0fd0a8a4d3509108
eliasnachle <elias.nachle@bandtec.com.br>
2021-11-08 03:00:19 -0300

    refactor(searchRestaurant): removendo duplicidade de modal e atribuindo a uma unica funcao

--- projeto-site/public/js/dashboard-support/search_restaurant.js
+++ projeto-site/public/js/dashboard-support/search_restaurant.js
@@ -23,52 +23,44 @@ Modal Restaurante
 ---------------*/
 [clickHandlerSearchRestaurant, clickHandlerMobileSearchRestaurant].map(element => element.addEventListener('click', () => {
     addOverflow();
-    const modalSearchRestaurant = document.createElement("section");
-    modalSearchRestaurant.classList.add('overlay__modal');
-    modalSearchRestaurant.innerHTML += `
-    <section class="overlay__modal">
-      <div class="overlay__modal--modal">
-        <div class="overlay__modal--modal--content">
-        <div class="overlay__modal--modal--content--close" onclick="closeModal()">
-          <svg fill="none" height="20" viewBox="0 0 19 19" width="20" xmlns="http://www.w3.org/2000/svg">
-              <path clip-rule="evenodd" d="M18.5578 18.5579C17.9682 19.1474 17.0124 19.1474 16.4228 18.5579L0.442158 2.57712C-0.147386 1.98757 -0.147386 1.03171 0.442158 0.442157C1.03171 -0.147388 1.98756 -0.147388 2.57711 0.442171L18.5578 16.4229C19.1474 17.0125 19.1474 17.9683 18.5578 18.5579Z" fill="#161616" fill-rule="evenodd"></path>
-              <path clip-rule="evenodd" d="M0.442158 18.5579C-0.147386 17.9683 -0.147386 17.0125 0.442158 16.4229L16.4228 0.442169C17.0124 -0.14739 17.9682 -0.14739 18.5578 0.442169C19.1474 1.03171 19.1474 1.98757 18.5578 2.57711L2.57711 18.5579C1.98756 19.1474 1.03171 19.1474 0.442158 18.5579Z" fill="#161616" fill-rule="evenodd"></path>
-          </svg>
-        </div>      
-          <img src="../imgs/search_restaurant.svg">
-          <h3>Qual unidade você quer encontrar?</h3>
-          <div class="overlay__modal--modal--content--ipt">
-            <i class="icon-search"></i>
-            <input type="text" id="ipt_search_restaurant" placeholder="Busque pelo nome da unidade">
-          </div>      
-          <button onclick="searchResraurant()" id="btn_search_restaurant">Buscar</button>
-          <span id="msg_validate_search_restaurant"></span>
-        </div>
-      </div>
-    </section>`; 
-    body.appendChild(modalSearchRestaurant);
+    modalRestaurant();
 }));
 // Modal restaurante nao setado
 if(sessionStorage.getItem("restaurantSession") == null){
-    addOverflow();
-    const modalSearchRestaurant = document.createElement("section");
-    modalSearchRestaurant.classList.add('overlay__modal');
-    modalSearchRestaurant.innerHTML += `
-    <section class="overlay__modal">
-      <div class="overlay__modal--modal">
-        <div class="overlay__modal--modal--content">
-          <img src="../imgs/search_restaurant.svg">
-          <h3>Qual unidade você quer encontrar?</h3>
-          <div class="overlay__modal--modal--content--ipt">
-            <i class="icon-search"></i>
-            <input type="text" id="ipt_search_restaurant" placeholder="Busque pelo nome da unidade">
-          </div>      
-          <button onclick="searchResraurant()" id="btn_search_restaurant">Buscar</button>
-          <span id="msg_validate_search_restaurant"></span>
-        </div>
+    addOverflow();    
+    modalRestaurant();
+}
+
+function modalRestaurant(){
+  const modalSearchRestaurant = document.createElement("section");
+  modalSearchRestaurant.classList.add('overlay__modal');
+  modalSearchRestaurant.innerHTML += `
+  <section class="overlay__modal">
+    <div class="overlay__modal--modal">
+      <div class="overlay__modal--modal--content">
+        <img src="../imgs/search_restaurant.svg">
+        <h3>Qual unidade você quer encontrar?</h3>
+        <div class="overlay__modal--modal--content--ipt">
+          <i class="icon-search"></i>
+          <input type="text" id="ipt_search_restaurant" placeholder="Busque pelo nome da unidade">
+        </div>      
+        <button onclick="searchResraurant()" id="btn_search_restaurant">Buscar</button>
+        <span id="msg_validate_search_restaurant"></span>
       </div>
-    </section>`;
-    body.appendChild(modalSearchRestaurant);
+    </div>
+  </section>`;
+  body.appendChild(modalSearchRestaurant);
+  if(sessionStorage.getItem("restaurantSession") !== null){
+    const modalContent = document.querySelector('.overlay__modal--modal--content');
+    modalContent.insertAdjacentHTML('beforebegin' ,`
+    <div class="overlay__modal--modal--content--close" onclick="closeModal()">
+      <svg fill="none" height="20" viewBox="0 0 19 19" width="20" xmlns="http://www.w3.org/2000/svg">
+        <path clip-rule="evenodd" d="M18.5578 18.5579C17.9682 19.1474 17.0124 19.1474 16.4228 18.5579L0.442158 2.57712C-0.147386 1.98757 -0.147386 1.03171 0.442158 0.442157C1.03171 -0.147388 1.98756 -0.147388 2.57711 0.442171L18.5578 16.4229C19.1474 17.0125 19.1474 17.9683 18.5578 18.5579Z" fill="#161616" fill-rule="evenodd"></path>
+        <path clip-rule="evenodd" d="M0.442158 18.5579C-0.147386 17.9683 -0.147386 17.0125 0.442158 16.4229L16.4228 0.442169C17.0124 -0.14739 17.9682 -0.14739 18.5578 0.442169C19.1474 1.03171 19.1474 1.98757 18.5578 2.57711L2.57711 18.5579C1.98756 19.1474 1.03171 19.1474 0.442158 18.5579Z" fill="#161616" fill-rule="evenodd"></path>
+      </svg>
+    </div> 
+    `);
+  }
 }
 // Validacao pesquisa resturante
 function searchResraurant(){
