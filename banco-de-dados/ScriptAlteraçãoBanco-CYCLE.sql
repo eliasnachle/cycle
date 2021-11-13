82567aa26a52e6edb6c3e77646965b86c506d37a
carloslimasousa <carlos.lsousa3@outlook.com>
2021-11-12 22:07:16 -0300

    Refactory: Mudanças do banco no DER e ScriptSql

--- banco-de-dados/ScriptAlteraçãoBanco-CYCLE.sql
+++ banco-de-dados/ScriptAlteraçãoBanco-CYCLE.sql
@@ -1,7 +1,3 @@
--- Configurando foreign key da tabela unidade
-ALTER TABLE tblUnidades ADD FOREIGN KEY (idUsuarioContratante) REFERENCES tblUsuariosContratante(idUsuarioContratante);
-ALTER TABLE tblUnidades ADD FOREIGN KEY (idCodigoUf) REFERENCES tblCodigosUf(idCodigoUf);
-
 -- Configurando foreign key da tabela contratante
 ALTER TABLE tblUsuariosContratante ADD FOREIGN KEY (idPlano) REFERENCES tblPlanos(idPlano);
 
@@ -9,7 +5,7 @@ ALTER TABLE tblUsuariosContratante ADD FOREIGN KEY (idPlano) REFERENCES tblPlano
 ALTER TABLE tblUsuariosSuporte ADD FOREIGN KEY (idUsuarioContratante) REFERENCES tblUsuariosContratante(idUsuarioContratante);
 
 -- Configurando foreign key da tabela maquina
-ALTER TABLE tblMaquinas ADD FOREIGN KEY (idUnidade) REFERENCES tblUnidades(idUnidade);
+ALTER TABLE tblMaquinas ADD FOREIGN KEY (idUsuarioContratante) REFERENCES tblUsuariosContratante(idUsuarioContratante);
 
 -- Configurando foreign key da tabela registro
 ALTER TABLE tblRegistros ADD FOREIGN KEY (idMaquina) REFERENCES tblMaquinas(idMaquina);
