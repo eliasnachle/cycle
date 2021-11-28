e79d77f5dcdea9a56202e40e7cb51d06e6dc92a1
eliasnachle <elias.nachle@bandtec.com.br>
2021-11-28 04:39:19 -0300

    feature: criando variavel para validar autenticacao

--- projeto-site/public/js/validation_login.js
+++ projeto-site/public/js/validation_login.js
@@ -4,7 +4,6 @@ password = document.querySelector('#ipt_password'),
 loginReg = /^([À-úA-z0-9._-]+@[a-z0-9._-]+\.[A-z0-9_-]+)$/,
 passwordReg = /^(?=.*[0-9]{1})(?=.*[\W]{1})(?=.*[a-z]{1})[a-zA-Z0-9\W]{6,30}$/;
 
-// Validação Login
 login.addEventListener('keyup', () => {    
     const iptLoginValue = login.value;
     if(iptLoginValue.match(loginReg)){
@@ -28,88 +27,57 @@ togglePassword.addEventListener('click', () => {
 
 function authenticateLogin() { 
     submit_button.disabled = true
-
     if (isGerenteOrSuporte.value == "Gerente") {
         var formLogin = new URLSearchParams(new FormData(form_login));
         fetch("/usuarios/login-contratante", {
             method: "POST",
             body: formLogin
         }).then(resposta => {
-
             if (resposta.ok) {
-
                 resposta.json().then(json => {
-
+                    localStorage.hasManagerSession = true;
                     sessionStorage.name_user_contratante = json.nomeContratante;
                     sessionStorage.email_user_contratante = json.emailContratante;
                     sessionStorage.idPlano_user_contratante = json.idPlano;
-
                     submit_button.disabled = false
-                    window.location.href = "dash_user_equipe.html";
-
+                    window.location.href = "dashboard-manager/dash_user_equipe.html";
                 });
-
             } else {
-
                 var erroMessage = "Usuario e/ou senha invalidos"
-
                 console.log(erroMessage);
-
                 login_error_message.innerHTML = erroMessage
                 login_error_message.style.display = "block"
-
                 submit_button.disabled = false
-
             }
         });
-
         return false;
-
     } else if (isGerenteOrSuporte.value == "Suporte") {
-
         var formLogin = new URLSearchParams(new FormData(form_login));
         fetch("/usuarios/login-suporte", {
             method: "POST",
             body: formLogin
         }).then(resposta => {
-
             if (resposta.ok) {
-
                 resposta.json().then(json => {
-
-                    sessionStorage.name_user_suporte = json.nomeSuporte;
-                    sessionStorage.email_user_suporte = json.emailSuporte;
-                    sessionStorage.idContratante_user_suporte = json.idUsuarioContratante;
-
-                    console.log(sessionStorage.name_user_suporte, sessionStorage.email_user_suporte, sessionStorage.idContratante_user_suporte)
-
+                    localStorage.hasSupportSession = true;
+                    localStorage.nameUserSupport = json.nomeSuporte;
                     window.location.href = "dashboard-support/dashboard.html";
                 });
-
             } else {
                 var erroMessage = "Usuario e/ou senha invalidos"
-
                 console.log(erroMessage);
-
                 login_error_message.innerHTML = erroMessage
                 login_error_message.style.display = "block"
-
                 submit_button.disabled = false
             }
         });
-
         return false;
-
     } else {
         var errorMessage = "Por favor selecione um tipo de login (Gerente ou Suporte)"
-
         console.log(errorMessage);
-
         login_error_message.innerHTML = errorMessage
         login_error_message.style.display = "block"
-
         submit_button.disabled = false
-
         return false;
     }
 }
