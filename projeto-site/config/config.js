fe7fff2b3ad7c40d0f32324029c7089640153126
Christian <christian.assis@bandtec.com.br>
2021-12-06 14:35:58 -0300

    comentando config localteste

--- projeto-site/config/config.js
+++ projeto-site/config/config.js
@@ -22,25 +22,25 @@ module.exports = {
     }
   },
 
-  // Insira aqui seus dados do banco LOCAL - MySQL Workbench
-  dev: {
-    username: 'root',
-    password: 'bandtec',
-    database: 'cycle',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    xuse_env_variable: 'DATABASE_URL',
-    dialectOptions: {
-      options: {
-        encrypt: true
-      }
-    },
-    pool: { 
-      max: 5,
-      min: 1,
-      acquire: 5000,
-      idle: 30000,
-      connectTimeout: 5000
-    }
-  },
-};
+//   // Insira aqui seus dados do banco LOCAL - MySQL Workbench
+//   dev: {
+//     username: 'root',
+//     password: 'bandtec',
+//     database: 'cycle',
+//     host: '127.0.0.1',
+//     dialect: 'mysql',
+//     xuse_env_variable: 'DATABASE_URL',
+//     dialectOptions: {
+//       options: {
+//         encrypt: true
+//       }
+//     },
+//     pool: { 
+//       max: 5,
+//       min: 1,
+//       acquire: 5000,
+//       idle: 30000,
+//       connectTimeout: 5000
+//     }
+//   },
+// };
